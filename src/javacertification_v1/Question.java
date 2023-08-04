71f99311d926edcef1e154f1e23e5d74af631216
N Zharinov <nikita.zharinov@gmail.com>
2023-08-04 10:32:08 +0200

    Commit 3. Questions class added. Change methods in Question class

--- src/javacertification_v1/Question.java
+++ src/javacertification_v1/Question.java
@@ -1,56 +1,51 @@
-
 package javacertification_v1;
 
 public class Question {
 	private String questionText;
 	private String[] answerOptions;
 	private String[] correctAnswers;
-	private String answerText;
+	private String explanation;
 	
-	public Question() {
-		System.out.println("Конструктор Question");
-	}
-	
-	
-
 	public Question(String questionText, String[] answerOptions, String[] correctAnswers, String answerText) {
 		this.questionText = questionText;
 		this.answerOptions = answerOptions;
 		this.correctAnswers = correctAnswers;
-		this.answerText = answerText;
+		this.explanation = answerText;
 	}
 
 	public String getQuestionText() {
 		return questionText;
 	}
 
-	public void setQuestionText(String questionText) {
-		this.questionText = questionText;
-	}
-
 	public String[] getAnswerOptions() {
 		return answerOptions;
-	}
-
-	public void setAnswerOptions(String[] answerOptions) {
-		this.answerOptions = answerOptions;
 	}
 
 	public String[] getCorrectAnswers() {
 		return correctAnswers;
 	}
 
-	public void setCorrectAnswers(String[] correctAnswers) {
-		this.correctAnswers = correctAnswers;
-	}
-
 	public String getExplanation() {
-		return answerText;
-	}
-
-	public void setExplanation(String explanation) {
-		this.answerText = explanation;
+		return explanation;
 	}
 	
+	public void printQuestionText() {
+		System.out.println(questionText);
+	}
 	
+	public void printAnswerOptions() {
+		for(String answer : answerOptions) {
+			System.out.println(answer);
+		}
+	}
+	
+	public void printCorrectAnswers() {
+		for(String answer : correctAnswers) {
+			System.out.println(answer);
+		}
+	}
+	
+	public void printExplanation() {
+		System.out.println(explanation);
+	}
 }
---
+++ src/javacertification_v1/Questions.java
@@ -0,0 +1,58 @@
+package javacertification_v1;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Questions {
+	Question[] questions;
+
+	Questions() {
+		ArrayList<Question> stackQuestions = readQuestionsFromFile();
+
+		questions = new Question[stackQuestions.size()];
+		questions = stackQuestions.toArray(questions);
+	}
+	
+	private ArrayList<Question> readQuestionsFromFile() {
+		ArrayList <Question> questions = new ArrayList();
+		Scanner scanner = null;
+		
+		try {
+			scanner = new Scanner(new FileReader("init.txt"));
+			scanner.useDelimiter("#");
+			while(scanner.hasNextLine()) {
+				String question = scanner.next();
+				scanner.skip(scanner.delimiter());
+				String answerOptions[] = scanner.next().split("%%");
+				scanner.skip(scanner.delimiter());
+				String correctAnswers[] = scanner.next().split("%%");
+				scanner.skip(scanner.delimiter());
+				String answerText = scanner.nextLine();				
+				questions.add(new Question(
+						question,
+						answerOptions,
+						correctAnswers,
+						answerText));
+			}
+		} catch(IOException e) {
+			
+		} finally {
+			if (scanner != null) {
+				scanner.close();
+			}
+		}
+		return questions;
+	}
+
+	public void runTest() {
+		
+        for (Question q : questions) {
+            q.printQuestionText();
+            q.printAnswerOptions();
+            q.printCorrectAnswers();
+            q.printExplanation();
+        }
+	}
+}
